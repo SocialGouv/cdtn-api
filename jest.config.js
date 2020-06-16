8fa59039b1d68e52157089c92b5b71fb618bc6cf
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-06-16 19:47:13 +0200

    x

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,6 @@ module.exports = {
   coveragePathIgnorePatterns: ["<rootDir>/packages/api/src/middlewares/router.js"],
   projects: ["<rootDir>/e2e/jest.config.js", "<rootDir>/src/jest.config.js"],
   transform: {
-    "^.+\\.[t|j]sx?$": "babel-jest",
+    "^.+\\.js$": "babel-jest",
   },
 };
