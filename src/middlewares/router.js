2714785c0484064c2ece04401f1289eae8668e0e
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-06-16 18:49:41 +0200

    fix: add /healthz

--- src/middlewares/router.js
+++ src/middlewares/router.js
@@ -2,11 +2,13 @@ import KoaRouter from "@koa/router";
 
 import AgreementController from "../controllers/Agreement";
 import ArticleController from "../controllers/Article";
+import HealthController from "../controllers/Health";
 import IndexController from "../controllers/Index";
 
 const router = new KoaRouter();
 
 router.get("/", IndexController.get);
+router.get("/healthz", HealthController.get);
 router.get("/agreement/:idOrIdcc", AgreementController.get);
 router.get("/agreements", AgreementController.index);
 router.get("/article/:id", ArticleController.get);
