d2ceb8eb07285292a47e341f93116352e21bb3d2
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-06-16 19:59:56 +0200

    fix(travis): add missing babel polyfill for e2e tests

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,7 @@ module.exports = {
   coverageDirectory: "<rootDir>/coverage",
   coveragePathIgnorePatterns: ["<rootDir>/packages/api/src/middlewares/router.js"],
   projects: ["<rootDir>/e2e/jest.config.js", "<rootDir>/src/jest.config.js"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
 };
