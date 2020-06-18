7c08ea3fcd5647fb2b44e81dc09216efb075f25c
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-06-18 18:33:47 +0200

    chore(ci): fix production url

--- .k8s/components/api.ts
+++ .k8s/components/api.ts
@@ -1,5 +1,18 @@
 import env from "@kosko/env";
+import assert from "assert";
 import { create } from "@socialgouv/kosko-charts/components/app";
 
 const { deployment, ingress, service } = create(env.component("api"));
+
+if (process.env.PRODUCTION) {
+  const defaultHost = ingress.spec!.rules![0]!;
+  assert(process.env.KUBE_INGRESS_BASE_DOMAIN);
+  ingress.spec!.rules!.push({
+    ...defaultHost,
+    host: process.env.KUBE_INGRESS_BASE_DOMAIN,
+  });
+
+  ingress.spec!.tls![0].hosts!.push(process.env.KUBE_INGRESS_BASE_DOMAIN);
+}
+
 export default [deployment, ingress, service];
