af5637ad1b930839bca357e9feec578439aa4ad6
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2020-07-06 04:01:50 +0200

    fix: disable cache seeding (#24)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import cors from "@koa/cors";
 import Koa from "koa";
 import log from "npmlog";
 
-import seedCache from "./helpers/seedCache";
+// import seedCache from "./helpers/seedCache";
 import router from "./middlewares/router";
 
 log.enableColor();
@@ -13,6 +13,6 @@ const PORT = Number(process.env.PORT || 3000);
 const app = new Koa();
 app.use(cors()).use(router.routes()).use(router.allowedMethods());
 
-seedCache();
+// seedCache();
 app.listen(PORT);
 log.info("api", "Listening on %s (%s).", PORT, NODE_ENV);
