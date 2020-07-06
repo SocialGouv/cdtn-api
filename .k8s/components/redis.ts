0562a4e6887fef26d73940cb283eed34ef7c13cb
Douglas Duteil <douglasduteil@gmail.com>
2020-07-06 17:13:22 +0200

    build(k8s): use dev subdomain

--- .k8s/components/redis.ts
+++ .k8s/components/redis.ts
@@ -1,7 +1,40 @@
 import env from "@kosko/env";
-import assert from "assert";
+import { ok } from "assert";
+import { merge } from "@socialgouv/kosko-charts/utils/merge";
 import { create } from "@socialgouv/kosko-charts/components/app";
+import { IIoK8sApiCoreV1Container } from "kubernetes-models/_definitions/IoK8sApiCoreV1Container";
 
 const { deployment, ingress, service } = create(env.component("redis"));
+
+const container: Omit<IIoK8sApiCoreV1Container, "name"> = {
+  livenessProbe: {
+    exec: {
+      command: ["sh", "-c", "redis-cli ping"],
+    },
+  },
+  readinessProbe: {
+    exec: {
+      command: ["sh", "-c", "redis-cli ping"],
+    },
+  },
+  startupProbe: {
+    exec: {
+      command: ["sh", "-c", "redis-cli ping"],
+    },
+  },
+};
+
+ok(deployment.spec);
+ok(deployment.spec.template.spec);
+ok(deployment.spec.template.spec.containers[0].livenessProbe);
+ok(deployment.spec.template.spec.containers[0].readinessProbe);
+ok(deployment.spec.template.spec.containers[0].startupProbe);
+delete deployment.spec.template.spec.containers[0].livenessProbe.httpGet;
+delete deployment.spec.template.spec.containers[0].readinessProbe.httpGet;
+delete deployment.spec.template.spec.containers[0].startupProbe.httpGet;
+deployment.spec.template.spec.containers[0] = merge(
+  deployment.spec.template.spec.containers[0],
+  container,
+);
 
 export default [deployment, ingress, service];
--- .k8s/environments/_base/redis.ts
+++ .k8s/environments/_base/redis.ts
@@ -1,19 +1,20 @@
 import { AppComponentEnvironment } from "@socialgouv/kosko-charts/components/app/params";
 import { ok } from "assert";
 
-ok(process.env.CI_REGISTRY_IMAGE);
 ok(process.env.CI_COMMIT_SHA);
+ok(process.env.CI_ENVIRONMENT_SLUG);
 ok(process.env.CI_PROJECT_NAME);
+ok(process.env.CI_REGISTRY_IMAGE);
 
 const env = {
   containerPort: 6379,
 
   image: {
     name: "redis",
-    tag: "latest",
+    tag: "6.0.5-alpine3.12",
   },
 
-  subdomain: `redis-${process.env.CI_PROJECT_NAME}`,
+  subdomain: `redis-${process.env.CI_ENVIRONMENT_SLUG}-${process.env.CI_PROJECT_NAME}`,
 
   ingress: {
     secretName: process.env.PRODUCTION ? "redis-crt" : "wildcard-crt",
