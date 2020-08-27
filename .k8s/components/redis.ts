81edeb3d07887c43c557285bcc6fe01277f78fdb
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-08-27 17:10:14 +0200

    fix(deploy): increase limits

--- .k8s/components/redis.ts
+++ .k8s/components/redis.ts
@@ -4,6 +4,20 @@ import { create } from "@socialgouv/kosko-charts/components/redis";
 
 const manifests = create({
   env,
+  deployment: {
+    container: {
+      resources: {
+        requests: {
+          cpu: "5m",
+          memory: "128Mi",
+        },
+        limits: {
+          cpu: "1000m",
+          memory: "1Gi",
+        },
+      },
+    },
+  },
 });
 
 export default manifests;
