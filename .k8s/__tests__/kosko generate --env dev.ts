902af32072c77ed604018f065b66460c1c09d81e
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-12-10 17:42:53 +0100

    fix: tests

--- .k8s/__tests__/kosko generate --env dev.ts
+++ .k8s/__tests__/kosko generate --env dev.ts
@@ -1,8 +1,9 @@
 //
 
 import { getEnvManifests } from "@socialgouv/kosko-charts/testing";
+import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --dev", async () => {
-  expect(await getEnvManifests("dev")).toMatchSnapshot();
+  expect(await getEnvManifests("dev", "", project("cdtn-api").dev)).toMatchSnapshot();
 });
