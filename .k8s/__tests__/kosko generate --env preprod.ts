7a3428c9150b4fea9c70a9e896059bd5c672ea41
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-12-17 12:35:58 +0100

    feat: upgrade CI deps (#60)
    * feat: upgrade CI deps
    
    * Delete redirect.yaml
    
    * fix: rm useless
    
    * fix: gitlab-ci-yml
    
    * fix: tests
    
    * fix package
    
    * upgrade ci

---
+++ .k8s/__tests__/kosko generate --env preprod.ts
@@ -0,0 +1,9 @@
+//
+
+import { getEnvManifests } from "@socialgouv/kosko-charts/testing";
+import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
+
+jest.setTimeout(1000 * 60);
+test("kosko generate --preprod", async () => {
+  expect(await getEnvManifests("preprod", "", project("cdtn-api").preprod)).toMatchSnapshot();
+});
