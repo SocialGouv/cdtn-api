04ab5395906b13361bd4325a4d3b0d000ecf2bee
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-05-03 21:30:51 +0200

    feat(ci): use harbor registry

--- .k8s/__tests__/kosko generate --env prod.ts
+++ .k8s/__tests__/kosko generate --env prod.ts
@@ -5,5 +5,6 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --prod", async () => {
+  process.env.HARBOR_PROJECT = "cdtn";
   expect(await getEnvManifests("prod", "'!(_*)'", project("cdtn-api").prod)).toMatchSnapshot();
 });
