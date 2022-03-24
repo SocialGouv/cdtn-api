86e5525952d2856571bf1782029602f69ebd6bd7
Martial Maillot <martial.maillot@gmail.com>
2022-03-24 20:28:58 +0100

    fix: add a base url for e2e tests

--- e2e/jest.setup.js
+++ e2e/jest.setup.js
@@ -4,8 +4,10 @@ import axios from "axios";
 
 const PORT = process.env.PORT || 3000;
 
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+
 global.axios = axios.create({
-  baseURL: `http://localhost:${PORT}`,
+  baseURL: BASE_URL,
 });
 
 jest.setTimeout(30000);
