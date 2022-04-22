637690d3bd3e8316f6f00f1e97b6584def00c5c6
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-04-22 17:34:41 +0200

    use patch

---
+++ .kube-workflow/patches/index.js
@@ -0,0 +1,23 @@
+const initScript = `set -e
+yarn cache:update
+`;
+
+module.exports = (manifests, values) => {
+  console.log("values", values);
+  for (const manifest of manifests) {
+    if (manifest.kind === "Deployment" && manifest.metadata.name === "app") {
+      manifest.spec.template.spec = {
+        ...manifest.spec.template.spec,
+        initContainers: [
+          {
+            name: "init",
+            image: manifest.spec.template.spec.containers[0].image,
+            env: { name: "REDIS_URL", value: "redis://redis:80" },
+            command: ["/bin/sh", "-c", initScript],
+          },
+        ],
+      };
+    }
+  }
+  return manifests;
+};
